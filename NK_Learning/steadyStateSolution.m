eabeca9349cedb7c2cbbb85c2f3ea95c8b8e2b58
unknown <kl307@CNU335CHWX.isad.isadroot.ex.ac.uk>
2015-10-28 16:00:56 +0000

    Steady state solution changed. with the tex file.

--- NK_Learning/steadyStateSolution.m
+++ NK_Learning/steadyStateSolution.m
@@ -21,14 +21,38 @@ alphaY  = paramValues.alphaY;
     
 % evaluate function
     
-L_init    = 1;
-Lst       = fzero(@solveLabour,L_init);
-kst       = (( (Xst/alpha)*(1/beta-1+delta) )^(1/(alpha-1)))*Lst;
-Yst       = (kst^alpha)*(Lst^(1-alpha));
-Invst     = kst*delta;
-cst       = Yst - Invst;
-rKst      = (1/beta) - 1 + delta;
-wst       = (1/Xst)*(1-alpha)*(kst^alpha)*(Lst^(1-alpha));
-Rst       = 1/beta;
-profit_st = (1-(1/Xst))*Yst;
-Tst       = cst+kst-wst*Lst-Rst*kst-profit_st; 
+% L_init    = 1;
+% Lst       = fzero(@solveLabour,L_init);
+% kst       = (( (Xst/alpha)*(1/beta-1+delta) )^(1/(alpha-1)))*Lst;
+% Yst       = (kst^alpha)*(Lst^(1-alpha));
+% Invst     = kst*delta;
+% cst       = Yst - Invst;
+% rKst      = (1/beta) - 1 + delta;
+% wst       = (1/Xst)*(1-alpha)*(kst^alpha)*(Lst^(1-alpha));
+% Rst       = 1/beta;
+% profit_st = (1-(1/Xst))*Yst;
+% Tst       = cst+kst-wst*Lst-Rst*kst-profit_st; 
+
+
+Lst       =  ((1-alpha)*(Xst/alpha*(1/beta-1+delta))^(alpha/(alpha-1))/(Xst*(Xst/alpha*(1/beta-1+delta)-delta)*(Xst/alpha*(1/beta-1+delta))^(1/(alpha-1))))^(1/eta);
+
+kst       =  (Xst/alpha*(1/beta-1+delta))^(1/(alpha-1))*Lst;
+
+Yst       =   kst^alpha*Lst^(1-alpha);
+
+Rst       =   1/beta;
+
+rKst      =  (1/beta) - 1 + delta;
+
+wst       =  (1-alpha)*kst^alpha*Lst^(-alpha)/Xst;
+
+Invst     =   kst*delta;
+
+cst       =   Yst - Invst;
+
+profit_st =   (1-(1/Xst))*Yst;
+
+Tst       =   0; 
+
+
+
